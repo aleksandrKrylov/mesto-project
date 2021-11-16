e134bf3a1b138a731965a3e4ddbe8da271798cde
Aleksandr Krylov <a.v.krylovv@yandex.ru>
2021-11-16 22:20:22 +0300

    refactor: дать более понятные имена для классов карточки с картинками

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+  
+
 const profileName = document.querySelector('.profile__name');
 const profileActivity = document.querySelector('.profile__activity');
 
@@ -24,6 +52,10 @@ function infoProfile () {
 function saveProfileInfo () {
     profileName.textContent = inputName.value;
     profileActivity.textContent = inputActivity.value;
+}
+
+function addCards() {
+    
 }
 //----------------------- Фенкции --------------------------/
 
