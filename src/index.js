a61858bc605a5a02ae3f1beda94ccc8e223a4f04
Aleksandr Krylov <a.v.krylovv@yandex.ru>
2022-02-03 00:09:52 +0300

    fix: исправить ошибки после проверки ревью

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import './pages/index.css'; 
 
+const initialCards = [
+    {
+      name: "Архыз",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
+    },
+    {
+      name: "Челябинская область",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg"
+    },
+    {
+      name: "Иваново",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg"
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg"
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg"
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg"
+    }
+  ]; 
+
 const buttonProfileEdit = document.querySelector(".profile__button-edit");
 const buttonAddCard = document.querySelector(".profile__button-add");
 
+const popups = document.querySelectorAll('.popup')
+
+const formProfile = document.querySelector(".form_type_profile");
+const formCardAdd = document.querySelector(".form_type_card");
 const popupProfile = document.querySelector(".popup_type_profile");
-const buttonFormSaveProfile = document.querySelector(".form__button_profile");
 
 const popupCard = document.querySelector(".popup_type_card");
 const inputTitle = document.querySelector(".form__input_type_title");
 const inputLink = document.querySelector(".form__input_type_link");
 const buttonFormCardAdd = document.querySelector(".form__button_card-add");
-const popupImg = document.querySelector(".popup_type_img");
+
+const cardsСontainer = document.querySelector(".cards");
 
 const validationConfig = {
     formSelector: ".form",
@@ -23,22 +54,20 @@ const validationConfig = {
 import { openPopup,  closePopup } from './components/utils.js';
 import { infoProfileForm, saveProfileInfo } from './components/modal.js';
 import { enableValidation, disabledButton } from './components/validate.js';
-import { addCards,  addCard } from './components/card.js';
+import { createCard } from './components/card.js';
 
 infoProfileForm ();
 enableValidation(validationConfig);
-addCards();
+initialCards.forEach(data => {
+    cardsСontainer.append(createCard(data.link, data.name));
+})
 
 // Обработчики для формы профиля  
 buttonProfileEdit.addEventListener("click", () => {
     openPopup(popupProfile);
 });
 
-popupProfile.querySelector(".popup__close").addEventListener("click", () => {
-    closePopup(popupProfile);
-});
-
-buttonFormSaveProfile.addEventListener("click", () => {
+formProfile.addEventListener("submit", () => {
     saveProfileInfo();
     closePopup(popupProfile);
 });
@@ -48,34 +77,22 @@ buttonAddCard.addEventListener("click", () => {
     openPopup(popupCard);
 });
 
-popupCard.querySelector(".popup__close").addEventListener("click", () => {
-    closePopup(popupCard);
-});
-
-buttonFormCardAdd.addEventListener("click", () => {
-    addCard(inputLink.value,inputTitle.value);
+formCardAdd.addEventListener("submit", () => {
+    cardsСontainer.prepend(createCard(inputLink.value,inputTitle.value));
     inputTitle.value = "";
     inputLink.value = "";
     disabledButton(buttonFormCardAdd, validationConfig.buttonDisabledClass)
     closePopup(popupCard);
 })
 
-// Обработчик для формы с карточкой  
-popupImg.querySelector(".popup__close").addEventListener("click", () => {
-    closePopup(popupImg);
-});
-
-Array.from(document.querySelectorAll('.popup')).forEach(popup => {
-    //Закрытие попапа кликом на оверлей
+// Обработчики для закрытие попапа кликом на оверлей и на крестик
+popups.forEach((popup) => {
     popup.addEventListener('click', (evt) => {
-        if (evt.target.classList.contains('popup')) {
-            closePopup(popup);
+        if (evt.target.classList.contains('popup_opened')) {
+            closePopup(popup)
         }
-    });
-    //Закрытие попапа нажатием на Esc
-    document.addEventListener('keydown', function (evt) {
-        if (evt.key == 'Escape') {
-            closePopup(popup);
+        if (evt.target.classList.contains('popup__close')) {
+            closePopup(popup)
         }
-      }); 
+    })
 });
